ffa99c0e0f5479ab4b3e259f7a4df325e6dee5d8
“mi <nataliasaxo@gmail.com>
2022-11-03 15:13:11 -0300

    clase completa

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,33 @@
-const subtitulo = querySelector(".subtitulo");
+const qs = (element) => document.querySelector(element);
+const qsa = (element) => document.querySelectorAll(element);
+const sbi = (element) => document.getElementById(element);
+
+
+
+
+const main = sbi("mainContainer");
+const subtitulo = qs(".subtitulo");
+const parrafos = qsa("p");
+const listado = qs("h2 a");
+
 
 let nombre = prompt("Cual es tu nombre?");
-
 subtitulo.textcontent += nombre ?  nombre : invitado; 
+listado.style.color = "#FFFF"
+
+let respuesta = confirm("¿Querés cambiar el color de fondo?");
+if(respuesta) {
+    qs("body").classList.add("fondo")
+}
+
+parrafos.forEach((parrafo,index) => {
+    if(index%2===0){
+        parrafo.classList.add("destacadoPar")
+    }else{
+        parrafo.classList.add("destacadoImpar")
+    }
+
+});
+
+
+
