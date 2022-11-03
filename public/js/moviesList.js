94ef06e8a1d5861053ab4d054f15f72e095d94d7
“mi <nataliasaxo@gmail.com>
2022-11-03 16:36:03 -0300

    completo

--- public/js/moviesList.js
+++ public/js/moviesList.js
@@ -1,3 +1,16 @@
 window.onload = function (){
     let body = document.querySelector("body");
+    let moviesListTitulo = document.querySelector(".moviesListTitulo");
+
+    let modo = confirm("Desea modo oscuro?");
+    if(modo){
+        body.style.backgroundColor = "#7f7f7f"
+        body.classList.add("fondoMoviesList");
+    }
+
+    console.log(body);
+    moviesListTitulo.innerHTML = "LISTADO DE PELICULAS";
+    moviesListTitulo.STYLE.COLOR = "WHITE";
+    moviesListTitulo.style.backgroundColor = "teal";
+    moviesListTitulo.style.padding = "20px";
 }
